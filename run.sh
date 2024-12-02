c4fd889b271ccba5eb5af2aa5a577e9e80312396
David Engel <dengel1392@gmail.comi>
2024-12-02 15:32:15 -0500

    Added capability to set user of the pipeline

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 #$ -e ./logs/
 #$ -o ./logs/
 
-module load R/4.4.0
 set -eux
+
+user="dengel"
 
 while IFS=$'\t' read -r v1 v2 v3 v4 v5 v6; do
 
@@ -25,27 +26,27 @@ while IFS=$'\t' read -r v1 v2 v3 v4 v5 v6; do
     if [[ "$v3" == "raw" ]]; then
         for i in {1..5}; do
             echo "Submitting qsub for Raw data (run $i): v1=$v1, v2=$v2, v3=$v3, v4=$v4 v5=${v5//,/} v6=$v6"
-            qsub -N "bigscale2_DEV3_${v1}_${v2}_${v3}_${v5//,/}_run${i}_$(date +%m-%d-%y_%I-%M-%S%p)" bigscale2_DEVer4.qsub $v1 $v2 $v3 $v4 $v5 $v6 $i
+            #qsub -N "bigscale2_DEV3_${v1}_${v2}_${v3}_${v5//,/}_run${i}_$(date +%m-%d-%y_%I-%M-%S%p)" bigscale2_DEVer4.qsub $v1 $v2 $v3 $v4 $v5 $v6 $i
         done
     else
         echo "Submitting qsub for Norm data: v1=$v1, v2=$v2, v3=$v3, v4=$v4 v5=${v5//,/} v6=$v6"
-        qsub -N "bigscale2_DEV3_${v1}_${v2}_${v3}_${v5//,/}_$(date +%m-%d-%y_%I-%M-%S%p)" bigscale2_DEVer4.qsub $v1 $v2 $v3 $v4 $v5 $v6
+        #qsub -N "bigscale2_DEV3_${v1}_${v2}_${v3}_${v5//,/}_$(date +%m-%d-%y_%I-%M-%S%p)" bigscale2_DEVer4.qsub $v1 $v2 $v3 $v4 $v5 $v6
     fi
 
 done <config.tsv
 
-#sleep 10
+sleep 10
 
 # Wait for all jobs to complete
 echo "Waiting for jobs to complete..."
-while [[ $(qstat -u dengel | grep -c "bigscale2_") -gt 0 ]]; do
+while [[ $(qstat -u $user | grep -c "bigscale2_") -gt 0 ]]; do
     echo "Jobs still in the queue. Sleeping for 60 seconds..."
-    qstat -u dengel  # Log the output of qstat to see the current job list
-    sleep 1200  # Check every 60 seconds
+    qstat -u $user  # Log the output of qstat to see the current job list
+    sleep 10  # Check every 60 seconds
 done
 echo "All jobs completed."
 
-output="~/Bigscale_pipe/output_paths.txt"
+bash run_aggr.sh
 
 
-Rscript ~/Bigscale_pipe/AggregateCentrality.R $output
+
